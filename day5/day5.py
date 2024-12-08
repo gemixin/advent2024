f1733137a9482be26fd0883eff726d2277506bf8
Gemma McLean <gemixin@gmail.com>
2024-12-08 20:32:19 +0000

    Day 5 completed

--- day5/day5.py
+++ day5/day5.py
@@ -14,7 +14,7 @@ updates = []
 section = 1
 
 # Read in the input file in two separate sections
-with open('day5/test_input.txt') as file_object:
+with open('day5/input.txt') as file_object:
     for line in file_object:
         # If the line is blank, switch to section 2
         if line == '\n':
@@ -35,46 +35,72 @@ with open('day5/test_input.txt') as file_object:
         elif section == 2:
             updates.append(list(map(int, line.rstrip('\n').split(','))))
 
-total = 0
-# Loop through updates
-for update in updates:
+
+def check_update(update):
+    '''
+    Returns True if the given update contains no errors.
+    Returns False if even one error is found.
+    An error is when a page number violates the rules by appearing before or
+    after a number it shouldn't do.
+    '''
     num_pages = len(update)
-    correct = True
     # For each page number in the current update
     for i in range(num_pages):
         # If any of the page numbers to the left or right appear in the opposite rule list
-        breach1 = any(item in update[0:i] for item in rules[update[i]][1])
-        breach2 = any(item in update[i+1:num_pages] for item in rules[update[i]][0])
-        if breach1 or breach2:
-            # The update contains a mistake and we should break
-            correct = False
-            break
-    # If we reach here without finding a mistake, it's a correct report
-    if correct:
-        # Add middle number of correct report to total
-        total += update[(len(update) - 1)//2]
+        error1 = any(item in update[0:i] for item in rules[update[i]][1])
+        error2 = any(item in update[i+1:num_pages] for item in rules[update[i]][0])
+        if error1 or error2:
+            # The update contains an error so immediately return False
+            return False
+    # If this code is reached, there are no errors so return True
+    return True
+
+
+total = 0
+# Loop through updates
+for update in updates:
+    # If update is correct
+    if check_update(update):
+        # Add middle number of correct update to total
+        total += update[(len(update) - 1) // 2]
 
 # Part one answer
 print(total)
 
+
+def recursive_sort(page, sorted_update, processed, relevant_rules):
+    ''' Recursively sort the pages of an update according to the rules. '''
+    # Skip already processed pages
+    if page in processed:
+        return  # Skip if already processed
+    # Track which pages have been processed
+    processed.add(page)
+    # Ensure all dependencies are placed before the current page
+    for before_page in relevant_rules[page][0]:
+        # Only consider relevant dependencies
+        if before_page in relevant_rules:
+            recursive_sort(before_page, sorted_update, processed, relevant_rules)
+    # Add the current page if it's not already in the sorted list
+    if page not in sorted_update:
+        sorted_update.append(page)
+
+
 total = 0
 # Loop through updates
 for update in updates:
-    num_pages = len(update)
-    fixed = False
-    # For each page number in the current update
-    for i in range(num_pages):
-        # If any of the page numbers to the left or right appear in the opposite rule list
-        breach1 = any(item in update[0:i] for item in rules[update[i]][1])
-        breach2 = any(item in update[i+1:num_pages] for item in rules[update[i]][0])
-        if breach1 or breach2:
-            # The update contains a mistake that we need to fix
-            # TODO Fix the update by moving the number
-            fixed = True
-    # If we fixed at least one mistake
-    if fixed:
-        # Add middle number of fixed report to total
-        total += update[(len(update) - 1)//2]
+    # If update contains at least one error
+    if not check_update(update):
+        # Empty list for newly sorted update
+        sorted_update = []
+        # Empty set to track which numbers have been processed
+        processed = set()
+        # Extract relevant rules for pages in this update
+        relevant_rules = {page: rules[page] for page in update}
+        # Reorder the incorrectly sorted update
+        for page in update:
+            recursive_sort(page, sorted_update, processed, relevant_rules)
+        # Add the middle number of the sorted update to the total
+        total += sorted_update[(len(sorted_update) - 1) // 2]
 
 # Part two answer
 print(total)
